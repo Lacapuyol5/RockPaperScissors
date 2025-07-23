fad9fbd9f36dad58d5e8cc4c15f1c31c56eb3690
Lacapuyol5 <fabiantoby80@gmail.com>
2025-07-23 13:18:40 +0100

    odin project

---
+++ script.js
@@ -0,0 +1,15 @@
+function getComputerChoice(){
+
+var compChoice = Math.random() * 3;
+if (compChoice <= 1) {
+    return = "rock";
+} else if (compChoice <= 2) {
+    return = "paper";
+} else {
+    return = "scissors";
+}
+
+
+
+
+};
